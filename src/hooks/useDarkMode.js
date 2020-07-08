c74ba83345b8e1eeff667f1eb876e1405c739a7c
jidelson <58210030+jidelson@users.noreply.github.com>
2020-07-08 14:55:43 -0700

    met mvp

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = (initialValue) => {
+    const [darkModeValue, setDarkModeValue] = useLocalStorage('darkModeIsOn', initialValue)
+
+    useEffect(() => {
+        if(darkModeValue){
+            document.querySelector('body').classList.add('dark-mode')   
+           }
+        else(document.querySelector('body').classList.remove('dark-mode'))
+    }, [darkModeValue])
+
+    return[darkModeValue, setDarkModeValue]
+}
+
+export default useDarkMode;
